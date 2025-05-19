254faf4256cf46648efb98c557e34f3119ef4332
sharma6754 <raunitsharmavnrvjiet@gmail.com>
2025-05-19 11:08:11 +0530

    Configure wouter with base path for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -10,10 +10,12 @@ import { GameProvider } from "@/hooks/useGameState";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home}/>
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/ReactionRush/">
+      <Switch>
+        <Route path="/" component={Home}/>
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
